059993a846c8438d7c244be050c9f4510454468c
dzul hilmi <dzulhilmi752@gmail.com>
2024-05-28 22:13:06 +0700

    first commit

---
+++ app/Http/Controllers/BarangController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Barang;
+use Illuminate\Http\Request;
+
+class BarangController extends Controller
+{
+    public function index()
+    {
+        $barang = Barang::get();
+
+        return view('barang.index',['data'=> $barang]);
+    }
+
+    public function tambah()
+    {
+        return view('barang.form');
+    }
+
+    public function simpan(Request $request)
+    {
+        $data =[
+            'kode_barang' => $request->kode_barang,
+            'nama_barang' => $request->nama_barang,
+            'berat_barang' => $request->berat_barang,
+            'harga' => $request->harga,
+            'jumlah' => $request->jumlah,
+        ];
+
+        Barang::create($data);
+        return redirect()->route('barang');
+    }
+    
+    public function edit($id)
+    {
+        $barang = Barang::find($id)->first();
+
+        return view('barang.form', ['barang'=>$barang]);
+    }
+
+    public function update($id, Request $request)
+    {
+        $data =[
+            'kode_barang' => $request->kode_barang,
+            'nama_barang' => $request->nama_barang,
+            'berat_barang' => $request->berat_barang,
+            'harga' => $request->harga,
+            'jumlah' => $request->jumlah,
+        ];
+
+        Barang::find($id)->update($data);
+        return redirect()->route('barang');
+    }
+
+    public function hapus($id)
+    {
+        barang::find($id)->delete();
+        return redirect()->route('barang');
+    }
+
+}
